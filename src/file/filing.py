984e8e741fbf427c5971d2e12e7a948806955497
Daniel <79935756+stackddev@users.noreply.github.com>
2021-09-14 14:49:54 -0700

    Add files via upload

---
+++ src/file/filing.py
@@ -0,0 +1,15 @@
+file = open("/Desktop/EX.txt")
+file.read()
+//Add file.readline() 
+//To let pyshell read line by line
+
+file.write("More examples") //Use this to write more text 
+file.close
+
+//When done, the code should look like this:
+
+file = open("/Desktop/EX.txt")
+file.readline()
+
+file.write("More examples")
+file.close()
