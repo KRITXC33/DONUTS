9a24315d5fbe4b029ed47365e5d89ac3bfeb5296
MeltiDev <79935756+MeltiDev@users.noreply.github.com>
2021-09-17 16:00:59 -0700

    Add files via upload

---
+++ lib/prints/basics2.py
@@ -0,0 +1,23 @@
+animal = "tiger"
+if animal = "tiger"
+print("This is not a tiger")
+
+sleep_hours = 10
+if sleep_hours > 10
+print("You had enough sleep")
+elif sleep_hours < 10
+print("You must be really grumpy")
+else:
+  print("I don't want to deal with you, go to bed")
+  
+money_amount = 100
+if money_amount > 100
+print("You ain't poor at all")
+elif money_amount < 100
+print("You are really poor, go get a job")
+
+discord_bot = "Dank Memer"
+if discord_bot = "Dank Memer"
+print("You are going to die")
+else:
+  print("You might die")
