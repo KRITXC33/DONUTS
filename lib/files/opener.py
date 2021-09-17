42fcee0e1149044c53d1f6dd5db42a722385d6dc
Meltii <79935756+MeltiDev@users.noreply.github.com>
2021-09-17 16:28:00 -0700

    Create opener.py

---
+++ lib/files/opener.py
@@ -0,0 +1,11 @@
+file = open("/desktop/opener.py")
+file.open 
+file.write("THIS IS A PYTHON OPENER")
+
+file.close
+
+if opener.py = open
+print("This is a Python desktop opener.")
+else:
+  print("Unexpected 404 error, this file has been purged")
+  
